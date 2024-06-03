0edf23fedbeb64ce3d1dee7966c3839c88429e1e
Rémy R <remy.rabischung@laplateforme.io>
2024-06-03 14:31:40 +0200

    Job 13 terminé

---
+++ Jour01/Job13/index.php
@@ -0,0 +1,22 @@
+<?php
+
+function occurrences($str, $char) {
+    $count = 0;
+    $length = strlen($str);
+    for ($i = 0; $i < $length; $i++) {
+        if ($str[$i] == $char) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+$str = "Bonjour";
+$char = "o";
+echo "Le nombre d'occurrences de '$char' dans '$str' est : " . occurrences($str, $char) . "<br>";
+
+$str = "plateforme";
+$char = "p";
+echo "Le nombre d'occurrences de '$char' dans '$str' est : " . occurrences($str, $char);
+
+?>
