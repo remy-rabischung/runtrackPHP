3760abb81e20c3721084e39497da520eaf4c6924
Rémy R <remy.rabischung@laplateforme.io>
2024-06-03 11:15:25 +0200

    Job 0 , 0.9 , et 1 terminés

---
+++ Jour01/Job1/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$platform = "LaPlateforme";
+echo $platform;
+echo "<br>";
+
+$greeting = "Vive";
+$exclamation = "!";
+echo $greeting . " " . $platform . " " . $exclamation;
+echo "<br>";
+
+$number = 6;
+echo $number;
+echo "<br>";
+
+$number += 4;
+echo $number;
+echo "<br>";
+
+$isTrue = true;
+echo $isTrue ? 'true' : 'false';
+echo "<br>";
+
+$isTrue = false;
+echo $isTrue ? 'true' : 'false';
+echo "<br>";
+
+?>
