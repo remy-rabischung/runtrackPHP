ab84b77a5b20ccd9a94eaab56b2cda167c6c45b1
Rémy R <remy.rabischung@laplateforme.io>
2024-06-03 11:20:01 +0200

    Job 2 terminé

---
+++ Jour01/Job2/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Hello all !"; // Ceci est un commentaire sur une seule ligne, style c++
+
+/* Ceci est un commentaire sur
+       plusieurs lignes */
+
+var_dump("Hello all !"); # Ceci est un commentaire style shell sur une seule ligne
+
+?>
